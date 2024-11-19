44c3ff7dd2bd1cf58e55b32bce0679f2c69e3ef3
melxwsss <meldiva1607@gmail.com>
2024-11-19 11:02:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bíblia',
+    'Quem traiu Jesus?',
+    'Judas'
+)
+
+criaCartao(
+    'Bíblia',
+    'Quem colocou os animais na arca?',
+    'Noé'
+)
+
+criaCartao(
+    'Bíblia',
+    'Em quantos dias Jesus ressucitou?',
+    '3 dias'
+)
+
+criaCartao(
+    'Bíblia',
+    'Quem Jesus ressucitou?',
+    'Lázaro'
+)
